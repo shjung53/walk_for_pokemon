ebb55f9c0c03a70aa99ec0b5e5788e3c570691e9
shjung53 <gcjung777@naver.com>
2023-11-17 09:59:50 +0900

    로딩 다이얼로그 완성

---
+++ app/src/main/java/com/ssafy/walkforpokemon/LoadingDialog.kt
@@ -0,0 +1,27 @@
+package com.ssafy.walkforpokemon
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.ssafy.walkforpokemon.databinding.DialogLoadingBinding
+
+class LoadingDialog : DialogFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        val binding = DialogLoadingBinding.inflate(layoutInflater, container, false)
+
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        Glide.with(this).load(R.raw.charmander).into(binding.loadingImage)
+
+        return binding.root
+    }
+}
